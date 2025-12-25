6c1e77519faa3ede4fca0bf3bab7aedb32bd8f03
Bilal Hassen <bilalhassen097@gmail.com>
2025-12-25 14:16:06 -0500

    Initial commit: clean starter + brand variables

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Sweet &amp; Savory</h1>
+      <p className="subtitle">Your app is ready. Start building your menu.</p>
+    </main>
+  )
+}
+
+export default App
