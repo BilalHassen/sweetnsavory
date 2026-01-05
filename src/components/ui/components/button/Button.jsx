276f0ddbacf67b859640fa168dc71961a102b24f
Bilal Hassen <bilalhassen097@gmail.com>
2026-01-04 23:18:27 -0500

    ui(button): support selectable state + add medium size

--- src/components/ui/components/button/Button.jsx
+++ src/components/ui/components/button/Button.jsx
@@ -1,7 +1,24 @@
 import "./Button.scss";
-function Button({ children, className, variant, size }) {
-  const classes = `btn btn--${variant} btn--${size}`
-  return <button className={classes}>{children}</button>;
+function Button({
+  children,
+  className = "",
+  variant,
+  size,
+  identity,
+  ...props
+}) {
+  console.log("props:", props.selectedValue);
+
+  console.log(props.selectedValue === identity);
+
+  const classes = `btn btn--${
+    props.selectedValue !== identity ? "inactive" : variant
+  } btn--${size} ${className}`.trim();
+  return (
+    <button className={classes} {...props}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
