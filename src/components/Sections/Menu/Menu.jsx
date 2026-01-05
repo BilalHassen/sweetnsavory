d40f085d4ba9b9193ea1d86d18a2a2fb16179510
Bilal Hassen <bilalhassen097@gmail.com>
2026-01-04 23:18:32 -0500

    menu: add section with category toggle and item list

--- src/components/Sections/Menu/Menu.jsx
+++ src/components/Sections/Menu/Menu.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
+import React, { useState } from "react";
+import InfoCard from "@/components/ui/components/InfoCard/InfoCard";
+import SectionHeader from "@/components/ui/components/SectionHeader/SectionHeader";
+import Button from "@/components/ui/components/button/Button";
+import "./Menu.scss";
+import { piesData } from "@/data/data";
 
 function Menu() {
+  const [selectedValue, setSelectedValue] = useState("savoury");
+
+  function handleSelection(value) {
+    if (value === "savoury") {
+      setSelectedValue("savoury");
+    } else if (value === "sweet") {
+      setSelectedValue("sweet");
+    }
+  }
+
+  const savouryMenu = piesData.filter((pies) => pies.category === "savoury");
+  const sweetMenu = piesData.filter((pies) => pies.category === "sweet");
+
+  const activeMenu = selectedValue === "savoury" ? savouryMenu : sweetMenu;
+
   return (
-    <section className="section">Menu</section>
-  )
+    <section className="menu">
+      <SectionHeader
+        eyebrow={"Our Menu"}
+        title={"Discover Our Pies"}
+        description={
+          "All pies are available for in-store pickup, delivery, or dine-in. Call ahead for large orders and custom requests."
+        }
+      />
+      <div className="menu__flexCon">
+      <div className="menu__btnContainer">
+        <Button
+          variant={"primary"}
+          size={"med"}
+          identity={"savoury"}
+          selectedValue={selectedValue}
+          onClick={() => handleSelection("savoury")}
+        >
+          <span className="menu__btnText">Savoury</span>
+        </Button>
+        <Button
+          variant={"green"}
+          size={"med"}
+          identity={"sweet"}
+          selectedValue={selectedValue}
+          onClick={() => handleSelection("sweet")}
+        >
+          <span className="menu__btnText">Sweets</span>
+        </Button>
+      </div>
+      </div>
+      <div className="menu__container">
+      {activeMenu.map((item) => {
+        return(
+        <ul className="menu__itemsContainer">
+          <InfoCard className="menu__card">
+            <div className="menu__txtContainer">
+                <h3 className="menu__itemName">{item.name}</h3>
+                <p className="menu__price">${item.price}</p>
+            </div>
+            <p className="menu__ingredients">{item.ingredients}</p>
+          </InfoCard>
+        </ul>
+        )
+      })}
+      </div>
+    </section>
+  );
 }
 
-export default Menu
+export default Menu;
