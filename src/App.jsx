2fb8f175bb524577585040f13c4d66442bee3342
Bilal Hassen <bilalhassen097@gmail.com>
2026-01-16 21:44:46 -0500

    Render Contact section in app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from '@/components/Hero/Hero'
 import FeaturedPies from './components/Sections/FeaturedPies/FeaturedPies'
 import About from './components/Sections/About/About'
 import Menu from './components/Sections/Menu/Menu'
+import Contact from "./components/Sections/Contact/Contact"
 function App() {
   return (
     
@@ -20,6 +21,7 @@ function App() {
       
      <About/>
      <Menu/>
+     <Contact/>
      
     </Layout>
   )
